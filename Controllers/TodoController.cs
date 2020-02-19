185743a1d8858ec14a7d0daff924198eae1971a2
gulbaharcelik <gulbahar.celik18@gmail.com>
2020-02-19 23:09:42 +0300

    second commit

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -14,38 +14,60 @@ namespace TodoApi.Controllers
     {
         private readonly todo_dbContext _context;
 
-        public TodoController(todo_dbContext context){
+        public TodoController(todo_dbContext context)
+        {
             _context = context;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
         {
-            var allTodoLidt = _context.Todo.ToList();
-            return allTodoLidt;
+            var allTodoList = _context.Todo.Where(todo => todo.Status > 0).ToList();
+            return allTodoList;
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Todo>> Get(int id)
         {
-            return null;
+            var todo = _context.Todo.Where(item => item.Id == id).FirstOrDefault();
+            return todo;
         }
 
         [HttpPost]
         public async Task<ActionResult<int>> Create(Todo todo)
         {
+            if (todo != null)
+            {
+                _context.Add(todo);
+                return _context.SaveChanges();
+            }
             return 0;
         }
 
         [HttpPatch]
         public async Task<ActionResult<int>> Update(Todo todo)
         {
+            var itemToUpdate = _context.Todo.Where(item => item.Id == todo.Id).FirstOrDefault();
+            if (itemToUpdate != null)
+            {
+                itemToUpdate.Status = todo.Status;
+                itemToUpdate.Text = todo.Text;
+
+                return _context.SaveChanges();
+            }
             return 0;
         }
 
         [HttpDelete("id")]
         public async Task<ActionResult<int>> Delete(int id)
         {
+            var itemToDelete = _context.Todo.Where(item => item.Id == id).FirstOrDefault();
+            
+            if(itemToDelete != null){
+                _context.Todo.Remove(itemToDelete);
+                return _context.SaveChanges();
+            }
+
             return 0;
         }
     }
